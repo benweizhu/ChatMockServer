aed901254656a65592ca9c92a8b84f72bd7a3370
benweizhu <benweizhu@gmail.com>
2018-03-04 17:28:06 +0800

    put name and avatar in properties

--- src/main/java/me/zeph/bot/service/MessagesService.java
+++ src/main/java/me/zeph/bot/service/MessagesService.java
@@ -5,12 +5,19 @@ import me.zeph.bot.model.Messages;
 import me.zeph.bot.model.User;
 import me.zeph.bot.repository.MessagesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class MessagesService {
+
+    @Value("${chat.bot.avatar}")
+    private String avatar;
+
+    @Value("${chat.bot.name}")
+    private String botName;
 
     @Autowired
     private MessagesRepository messagesRepository;
@@ -22,11 +29,11 @@ public class MessagesService {
     public Messages saveMessagesFromUserAndBot(String username, Message message, String botReply) {
         Messages messages = messagesRepository.findByUsername(username);
         messages.getMessages().add(message);
-        messages.getMessages().add(getMessage(botReply));
+        messages.getMessages().add(getBotMessage(botReply));
         return messagesRepository.save(messages);
     }
 
-    private Message getMessage(String botReply) {
-        return new Message("", botReply, new Date(), new User());
+    private Message getBotMessage(String botReply) {
+        return new Message("", botReply, new Date(), new User("", botName, avatar));
     }
 }
