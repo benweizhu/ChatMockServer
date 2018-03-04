e273331110ecd2efb5e874583cf248e2a9d8c55b
benweizhu <benweizhu@gmail.com>
2018-03-04 16:46:33 +0800

    save message

--- src/main/java/me/zeph/bot/Application.java
+++ src/main/java/me/zeph/bot/Application.java
@@ -3,11 +3,21 @@ package me.zeph.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
 
+    private static final String ASIA_SHANGHAI = "Asia/Shanghai";
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(ASIA_SHANGHAI));
     }
 
 }
--- src/test/java/me/zeph/bot/service/MessagesServiceTest.java
+++ src/test/java/me/zeph/bot/service/MessagesServiceTest.java
@@ -1,54 +1,35 @@
-package me.zeph.bot.repository;
+package me.zeph.bot.service;
 
+import me.zeph.bot.Application;
 import me.zeph.bot.model.Message;
 import me.zeph.bot.model.Messages;
 import me.zeph.bot.model.User;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
-@DataMongoTest
+@SpringBootTest(classes = Application.class)
 @ActiveProfiles(value = "test")
-public class MessagesRepositoryTest {
+public class MessagesServiceTest {
 
     private static final String USERNAME = "benwei";
 
     @Autowired
-    private MessagesRepository messagesRepository;
-
-    @Before
-    public void setUp() {
-        messagesRepository.save(getMessages());
-    }
+    private MessagesService messagesService;
 
     @Test
-    public void shouldGetMessagesByUsername() {
-        Messages messages = messagesRepository.findByUsername(USERNAME);
+    public void shouldSaveMessage() {
+        Messages messages = messagesService.saveMessagesByUsername(USERNAME, getMessage("hi"));
         assertThat(messages.getUsername(), is(USERNAME));
-    }
-
-    private Messages getMessages() {
-        Messages messages = new Messages();
-        messages.setUsername(USERNAME);
-        messages.setMessages(getMessageList());
-        return messages;
-    }
-
-    private ArrayList<Message> getMessageList() {
-        ArrayList<Message> messageArrayList = new ArrayList<>();
-        messageArrayList.add(getMessage("hi"));
-        return messageArrayList;
     }
 
     private Message getMessage(String text) {
