438cacfbbd569540a7d744ec8c932f0e9ea1f9dd
benweizhu <benweizhu@gmail.com>
2018-03-04 11:53:41 +0800

    enable mongo db connection

---
+++ src/main/java/me/zeph/bot/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package me.zeph.bot.repository;
+
+import me.zeph.bot.model.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+
+    public List<Customer> findByLastName(String lastName);
+
+}
